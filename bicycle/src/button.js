b5e57389690e813ebd89570288b8b8da95c8ce00
honzaphan <honzaphan98@gmail.com>
2023-06-09 22:32:45 +0200

    Initial commit
    Created files CV, bicycle and starting to work on the 1st responsive website project.

---
+++ bicycle/src/button.js
@@ -0,0 +1,11 @@
+function redirect() {
+    location.href = "https://i.ytimg.com/vi/ImXoFFyTKVU/maxresdefault.jpg";
+}
+
+document.getElementById("myButton").onclick = redirect;
+
+// function redirect()
+// {
+// var url = "http://www.(url).com";
+// window.location(url);
+// }
